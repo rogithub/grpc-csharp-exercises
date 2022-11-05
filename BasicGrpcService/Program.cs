75dea8b3de303c4fb0ede2f13b4bdf95cb8d1ea8
rodrigo debian <correo.rodrigo@gmail.com>
2022-11-05 14:21:47 -0600

    basic template

--- BasicGrpcService/Program.cs
+++ BasicGrpcService/Program.cs
@@ -1,9 +1,15 @@
+using BasicGrpcService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
+
+app.UseEndpoints(endpoints => endpoints.MapGrpcService<GreetingsManagerService>());
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ BasicGrpcService/Services/GreetingsManagerService.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Grpc.Core;
+namespace BasicGrpcService
+{
+    public class GreetingsManagerService :
+    GreetingsManager.GreetingsManagerBase
+    {
+        public override Task<GreetingResponse>
+        GenerateGreeting(GreetingRequest request,
+        ServerCallContext context)
+        {
+            return Task.FromResult(new GreetingResponse
+            {
+                GreetingMessage = "Hello " + request.Name
+            });
+        }
+    }
+}
