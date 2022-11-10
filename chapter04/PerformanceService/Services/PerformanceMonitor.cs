f089d817c43a127369ad6cc117e43e0bc12470ed
rockdrigo <correo.rodrigo@gmail.com>
2022-11-10 16:36:01 -0600

    chapters

---
+++ chapter04/PerformanceService/Services/PerformanceMonitor.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Performance;
+
+namespace PerformanceService;
+public class PerformanceMonitor : Monitor.MonitorBase
+{
+    public override Task<PerformanceStatusResponse> GetPerformance(PerformanceStatusRequest request, ServerCallContext context)
+    {
+        var randomNumberGenerator = new Random();
+        return Task.FromResult(new PerformanceStatusResponse
+        {
+            CpuPercentageUsage =
+            randomNumberGenerator.NextDouble() * 100,
+            MemoryUsage = randomNumberGenerator.NextDouble() * 100,
+            ProcessesRunning = randomNumberGenerator.Next(),
+            ActiveConnections = randomNumberGenerator.Next()
+        });
+        
+    }
+}
