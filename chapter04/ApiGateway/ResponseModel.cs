93b51794f26c6575c0b370f5e8862b9f905caf95
rockdrigo <correo.rodrigo@gmail.com>
2022-11-10 16:59:28 -0600

    chapters

---
+++ chapter04/ApiGateway/ResponseModel.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+namespace ApiGateway;
+
+
+public class ResponseModel
+{
+    public List<PerformanceStatusModel> PerformanceStatuses { get; } = new();
+    public double RequestProcessingTime { get; set; }
+    public class PerformanceStatusModel
+    {
+        public double CpuPercentageUsage { get; set; }
+        public double MemoryUsage { get; set; }
+        public int ProcessesRunning { get; set; }
+        public int ActiveConnections { get; set; }  
+    }
+}
