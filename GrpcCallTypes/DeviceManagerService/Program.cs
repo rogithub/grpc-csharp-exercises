2d7c36ca34cfa4466db6a0c6006ea838fc090d7a
rockdrigo <correo.rodrigo@gmail.com>
2022-12-07 16:29:02 -0600

    settup chapter 07

--- GrpcCallTypes/DeviceManagerService/Program.cs
+++ GrpcCallTypes/DeviceManagerService/Program.cs
@@ -13,13 +13,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddSingleton<IDeviceStatusCache,DeviceStatusCache>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<ManagerService>();
 app.MapGrpcService<ManagerServiceCsNamespace>();
-builder.Services.AddSingleton<IDeviceStatusCache,DeviceStatusCache>();
+
 //app.MapGrpcService<ManagerServiceNoPackage>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
